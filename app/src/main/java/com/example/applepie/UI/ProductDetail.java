40a9315f97e72278f9514a6689a56780691854f3
Dung12309213 <dungctt22411ca@st.uel.edu.vn>
2025-06-02 15:29:50 +0700

    product detail

---
+++ app/src/main/java/com/example/applepie/UI/ProductDetail.java
@@ -0,0 +1,37 @@
+package com.example.applepie.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.applepie.R;
+
+public class ProductDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_product_detail);
+
+        // Áp padding cho system bar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Nếu bạn muốn xử lý click trong đây (ví dụ nút MUA NGAY)
+        findViewById(R.id.btnBuyNow).setOnClickListener(v -> {
+            // TODO: Mở khung chọn dung tích, số lượng, hoặc chuyển đến thanh toán
+        });
+    }
+
+
+}
